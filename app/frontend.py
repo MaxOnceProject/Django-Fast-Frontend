68ec527449e3b43575188b5a43a0b47788915cef
mnindel <maximilian.nindel@dje.de>
2023-04-10 12:14:01 +0200

    enhance table with pagination and search bar

--- app/frontend.py
+++ app/frontend.py
@@ -4,13 +4,21 @@ from app.models import Author
 
 # Register your models here.
 class AuthorFrontend(frontend.ModelFrontend):
-    fields = ('name', 'title')
-    inline_button = 'check'
+    # fields = ('name', 'title')
+    table_inline_button = ('check', 'uncheck')
     table_add = False
     table_show = True
+    # list_filter = ('name', 'title')
+    search_fields = ('name', 'title')
 
 
-    def post_inline_button(self, object):
+    def check(self, object):
         print(object.name)
+
+    def uncheck(self, object):
+        print(object.title)
+
+    def queryset(self):
+        pass
 
 frontend.site.register(Author, AuthorFrontend)
--- frontend/views.py
+++ frontend/views.py
@@ -2,13 +2,22 @@
 from django.conf import settings
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
+from django_filters import ChoiceFilter
+
 from .forms import generate_form_for_model
 from . import site
 from django.apps import apps
+from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 def favicon_view(request):
-    return {}
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+def create_filter_class(model, filters):
+    Meta = type("Meta", (), {"model": model, "fields": filters})
+    filter_class = type(f"{model.__name__}Filter", (django_filters.FilterSet,), {"Meta": Meta})
+    return filter_class
 
 def frontend_view(request, app_name=None, model_name=None, action=None, id=None):
     register = site.create_navbar_register()
@@ -55,12 +64,12 @@ def frontend_view(request, app_name=None, model_name=None, action=None, id=None)
     form_class = generate_form_for_model(model)
 
     if request.method == "POST":
-        if action == 'inline_button':
+        if action in getattr(frontend_config, 'table_inline_button'):
             model = apps.get_model(app_name, model_name)
             frontend_config = site._registry[model].__class__
 
             object = model.objects.get(id=id)
-            frontend_config().post_inline_button(object)
+            getattr(frontend_config(), action)(object)
 
         if action == 'table_add':
             form = form_class(request.POST)
@@ -69,14 +78,31 @@ def frontend_view(request, app_name=None, model_name=None, action=None, id=None)
 
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
-    fields = list(frontend_config.fields)
+    fields = list(getattr(frontend_config, 'fields', []))
 
-    if 'id' in frontend_config.fields:
+    if 'id' in fields:
         objects = model.objects.values(*fields)
+    elif not fields:
+        objects = model.objects.values()
     else:
         objects = model.objects.values(*fields, 'id')
 
-    if getattr(frontend_config, 'inline_button', False):
+    # Search
+    search_fields = getattr(frontend_config, 'search_fields', None)
+    if search_fields:
+        search_query = request.GET.get("q", "")
+        frontend_config.search_fields_option = True
+        if search_query:
+            search_query_list = [Q(**{f"{field}__icontains": search_query}) for field in search_fields]
+            objects = objects.filter(Q(*search_query_list, _connector=Q.OR))
+
+    # Pagination
+    paginator = Paginator(objects, 10)  # Show x items per page
+    page = request.GET.get("page")
+    objects = paginator.get_page(page)
+
+
+    if getattr(frontend_config, 'table_inline_button', False):
         frontend_config.inline_button_option = True
         fields += ['action']
 
@@ -90,14 +116,15 @@ def frontend_view(request, app_name=None, model_name=None, action=None, id=None)
             "table": {
                 "add": False,
                 "show": True,
-                "inline_button": getattr(frontend_config, 'inline_button_option', False)
+                "search": getattr(frontend_config, 'search_fields_option', False),
+                "inline_button": getattr(frontend_config, 'inline_button_option', False),
             },
         },
         "table": {
             "form": form_class(),
             "objects": objects,
             "fields": fields,
-            "inline_button": getattr(frontend_config, 'inline_button', None),
+            "inline_button": getattr(frontend_config, 'table_inline_button', None),
         }
     }
     return render(request, "frontend/site.html", context)
